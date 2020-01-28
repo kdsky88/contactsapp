a2259b367749854fd799e9d4de0cd882eebb0b4c
kdsky88 <kdsky88@gmail.com>
2020-01-28 18:02:49 +0900

    create contactsapp

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            '/api': {
+                //target: 'http://localhost:3000',
+                target: 'http://sample.bmaster.kro.kr',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+    }
+}
