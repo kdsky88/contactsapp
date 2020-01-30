d1d6a763ac4ea07fc7a696dce9a6b96df9d5d316
kdsky88 <kdsky88@gmail.com>
2020-01-30 14:08:28 +0900

    add vuex

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -28,22 +28,11 @@
 </template>
 
 <script>
-import eventBus from '../EventBus.js';
+import Constant from '../Constant';
+import { mapState } from 'vuex';
 
 export default {
     name : "contactForm"    ,
-    props : {
-        mode : {type:String, default:'add'},
-        contact : {
-            type :Object,
-            default: function() {
-                return {no:'', name:'', tel:'', address:'', photo: ''}
-            }
-        }
-    },
-    mounted : function() {
-        this.$refs.name.focus()
-    },
     computed : {
         btnText : function() {
             if (this.mode != 'update') return '추 가';
@@ -52,18 +41,22 @@ export default {
         headingText : function() {
             if (this.mode != 'update') return '새로운 연락처 추가';
             else return '연락처 변경';
-        }
+        },
+        ...mapState([ 'mode','contact'])
+    },
+    mounted : function() {
+        this.$refs.name.focus()
     },
     methods : {
         submitEvent : function() {
             if (this.mode == "update") {
-                eventBus.$emit("updateSubmit", this.contact);
+                this.$store.dispatch(Constant.UPDATE_CONTACT);
             } else {
-                eventBus.$emit("addSubmit", this.contact);
+                this.$store.dispatch(Constant.ADD_CONTACT);
             }
         },
         cancelEvent : function() {
-            eventBus.$emit("cancel");
+            this.$store.dispatch(Constant.CANCEL_FORM);
         }
     }
 }
